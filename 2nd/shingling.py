bd329d7e5f63a0a18f3013bd08d67e3a641e03e7
Tinge Zhang <tinge.zhang@gmail.com>
2014-10-17 00:22:36 +0800

    add shingling, minhashing and LSH impl

---
+++ 2nd/shingling.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+'''
+input:
+    1. documents,
+    1. shingles len
+    1. stop character list
+    1. output document
+output:
+    token list(CRC32) into ouput document in binary format
+'''
+
+
+def shingling(inputdoc, shinglen, stoplist):
+    return []
+
+
+if '__main__' == __name__:
+
+    return
+
+
+
